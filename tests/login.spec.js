3fbc57e3f3912993e80e979640f41868bee7dc62
unknown <neenukaduvanal123@gmail.com>
2026-07-14 10:01:56 +0530

    Login page is tested using page object modal

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,12 +1,13 @@
 import { test, expect } from '@playwright/test';
-
+import { LoginPage } from '../pages/login';
 test('login flow', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/login', {
-    waitUntil: 'domcontentloaded',
-  });
+  const Login = new LoginPage(page);
 
-  await page.getByLabel('Username').fill('tomsmith');
-  await page.getByLabel('Password').fill('SuperSecretPassword!');
-  await page.getByRole('button', { name: /login/i }).click();
-  await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible();
+  await Login.gotoLoginPage();
+  await Login.login('tomsmith', 'SuperSecretPassword!');
+
+  // await page.getByLabel('Username').fill('tomsmith');
+  // await page.getByLabel('Password').fill('SuperSecretPassword!');
+  // await page.getByRole('button', { name: /login/i }).click();
+  // await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible();
 });
